753f6078fd91f0d371ee2d121a2d05c7d67ff7d3
Ben Wibking <benwibking@gmail.com>
2020-09-02 12:56:31 +1000

    added serialization for boost rng

--- src/sfr.cpp
+++ src/sfr.cpp
@@ -11,8 +11,51 @@ auto main(int argc, char** argv) -> int {
     }
 }
 
+/* Writes a boost::random::mersenne_twister_engine to a @c std::ostream */
+template <class CharT, class Traits>
+friend std::basic_ostream<CharT, Traits> &
+operator<<(std::basic_ostream<CharT, Traits> &os,
+           const boost::random::mersenne_twister_engine &mt)
+{
+    UIntType data[n];
+    for (std::size_t j = 0; j < i; ++j)
+    {
+        data[j + n - i] = x[j];
+    }
+    if (i != n)
+    {
+        rewind(&data[n - i - 1], n - i);
+    }
+    os.write(reinterpret_cast<char*>(&data[0]), sizeof(data[0]));
+    for (std::size_t j = 1; j < n; ++j)
+    {
+        // write in binary format
+        os.write(reinterpret_cast<char*>(&data[j]), sizeof(data[j]);
+    }
+    return os;
+}
+
+/* Reads a boost::random::mersenne_twister_engine from a @c std::istream */
+template <class CharT, class Traits>
+friend std::basic_istream<CharT, Traits> &
+operator>>(std::basic_istream<CharT, Traits> &is,
+           boost::random::mersenne_twister_engine &mt)
+{
+    for (std::size_t j = 0; j < mt.state_size; ++j)
+    {
+        // read from binary format
+        is.read(reinterpret_cast<char*>(&mt.x[j]), sizeof(mt.x[j]));
+    }
+    
+    mt.i = mt.state_size; // this is to work around a compiler bug w/r/t templates
+    return is;
+}
+
 auto test_slug() -> bool
 {
+    // boost::random serialization test
+
+
     //Slug part
     double particle_mass = 100.0; // solar masses
     slug_object *SlugOb = slug_object_new();
