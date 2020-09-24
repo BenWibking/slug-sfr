dce6002a25751a88a1219210b3544d19c763c8a8
Ben Wibking <benwibking@gmail.com>
2020-09-24 14:51:08 +1000

    updated to use BLT for external libraries

---
+++ ubuntu_config.cmake
@@ -0,0 +1,3 @@
+set(ENABLE_MPI ON CACHE BOOL "")
+set(MPI_C_COMPILER "/home/ubuntu/openmpi3_custom/bin/mpicc" CACHE PATH "")
+set(MPI_CXX_COMPILER "/home/ubuntu/openmpi3_custom/bin/mpicxx" CACHE PATH "")
