d0cacb96dd30dc6165093270ece9137c4d2a72c3
Ben Wibking <benwibking@gmail.com>
2020-09-28 22:05:34 +1000

    update build system to work better

---
+++ ubuntu_amd_config.cmake
@@ -0,0 +1,6 @@
+set(CMAKE_C_COMPILER "/opt/AMD/aocc-compiler-2.2.0/bin/clang" CACHE PATH "")
+set(CMAKE_CXX_COMPILER "/opt/AMD/aocc-compiler-2.2.0/bin/clang++" CACHE PATH "")
+
+set(ENABLE_MPI ON CACHE BOOL "")
+set(MPI_C_COMPILER "/home/benwibking/spack/opt/spack/linux-ubuntu20.04-zen2/gcc-9.3.0/openmpi-3.1.6-nf53g4u4gdbvxf6nbxoc3w32yf5t5ls4/bin/mpicc" CACHE PATH "")
+set(MPI_CXX_COMPILER "/home/benwibking/spack/opt/spack/linux-ubuntu20.04-zen2/gcc-9.3.0/openmpi-3.1.6-nf53g4u4gdbvxf6nbxoc3w32yf5t5ls4/bin/mpicxx" CACHE PATH "")
