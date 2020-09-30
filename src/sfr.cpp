25fa928f38292e6d77d2802384092006b4746c10
Ben Wibking <benwibking@gmail.com>
2020-09-30 17:51:56 +1000

    updated slug version; use linux-clang Makefile for slug

--- src/sfr.cpp
+++ src/sfr.cpp
@@ -9,27 +9,22 @@ TEST(SlugObjectTest, SerializesDeserializes)
   slug_object *SlugOb = slug_object_new();
   slug_construct_cluster(SlugOb, particle_mass);
 
+  // serialize using old method
   size_t dimBuf = slug_buffer_size(SlugOb);
   char *buf_slug = (char *)malloc(dimBuf);
   slug_pack_buffer(SlugOb, buf_slug);
 
   slug_object *new_SlugOb = slug_object_new();
   slug_reconstruct_cluster(new_SlugOb, buf_slug);
+
+  // serialize reconstructed object using old method
   size_t dimBufNew = slug_buffer_size(new_SlugOb);
   char *buf_slugNew = (char *) malloc(dimBufNew);
   slug_pack_buffer(new_SlugOb, buf_slugNew);
 
+  // check whether new object and old object are equal
   ASSERT_EQ(dimBuf, dimBufNew);
   for(size_t i=0; i < dimBuf; ++i) {
     ASSERT_EQ(buf_slug[i], buf_slugNew[i]);
   }
-
-  delete SlugOb;
-  delete new_SlugOb;
-}
-
-// otherwise the SLUG main() function is used
-int main(int argc, char **argv) {
-  ::testing::InitGoogleTest(&argc, argv);
-  return RUN_ALL_TESTS();
 }
