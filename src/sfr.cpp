7fbcdd2639df1431c0c7b39e771998bf2bcd779a
Ben Wibking <benwibking@gmail.com>
2020-10-04 19:41:43 +1100

    added workaround for yields; use template magic to compare internal data of slug_cluster objects

--- src/sfr.cpp
+++ src/sfr.cpp
@@ -6,52 +6,87 @@ TEST(SlugObjectTest, SerializesDeserializes)
   // create slug_cluster object
   constexpr double particle_mass = 5.0e3; // solar masses
   constexpr double dt = 1.0e6; // years
+  double t = 0.; // years
 
   slug_object *SlugOb = slug_object_new();
   slug_construct_cluster(SlugOb, particle_mass);
   std::cout << "Constructed cluster of mass = " << particle_mass << " Msun." << std::endl;
 
-  constexpr int max_timesteps = 10;
-  for(int i = 0; i < max_timesteps; ++i) {
-    std::cout << "Advancing star cluster by dt = " << dt << std::endl;
-    slug_advance(SlugOb, dt);
-  }
+  constexpr int max_timesteps = 5;
+  for (int i = 0; i < max_timesteps; ++i)
+  {
+    t += dt;
+    slug_advance(SlugOb, t);
 
-  // serialize using old method
-  size_t dimBuf = slug_buffer_size(SlugOb);
-  char *buf_slug = (char *)malloc(dimBuf);
-  slug_pack_buffer(SlugOb, buf_slug);
+    // cluster->get_yield() has side effects -- this is a bug
+    SlugOb->cluster->get_yield(); // this *must* be called, otherwise
+                                  // yields will not be calculated from the previous timestep!
+  }
+  std::cout << "Advanced star cluster to time t = " << t << std::endl;
 
   // serialize using new method
   slug_cluster_state<NISO_SUKHBOLD16> state;
-  std::cout << "slug_cluster_state<" << NISO_SUKHBOLD16 << "> is " << sizeof(state) << " bytes." << std::endl;
   SlugOb->serialize_cluster_to_struct(state);
 
+  // print sizes of structs (in bytes)
   slug_cluster_state<2> state_small;
+  std::cout << "slug_cluster_state<" << NISO_SUKHBOLD16 << "> is " << sizeof(state) << " bytes." << std::endl;
   std::cout << "slug_cluster_state<2> is " << sizeof(state_small) << " bytes." << std::endl;
 
   // deserialize using new method
   slug_object *new_SlugOb = slug_object_new();
   new_SlugOb->reconstruct_cluster_from_struct(state);
 
+  // check whether old and new object are equal using operator==
+  //EXPECT_EQ((*new_SlugOb->cluster), (*SlugOb->cluster));
+  auto oldData = SlugOb->cluster->tied();
+  auto newData = new_SlugOb->cluster->tied();
+  for_each2(oldData, newData, [](auto index, auto &&oldDataElem, auto &&newDataElem) {
+    EXPECT_EQ(oldDataElem, newDataElem) << "Tuple elements at index "
+                      << index
+                      << " (type = "
+                      << type_name<decltype(oldDataElem)>()
+                      << ") are not equal!";
+  });
+
+  constexpr auto stars_index = 32;
+  auto printStars = [](std::vector<double> stars)
+  {
+    for (size_t i = 0; i < (stars.size() - 1); ++i)
+    {
+      std::cout << stars[i] << ", ";
+    }
+    std::cout << stars[stars.size()-1];
+    std::cout << std::endl;
+  };
+
+  // print out vector of stellar masses (stochastic part)
+#if 0
+  auto oldStars = std::get<0>(std::get<stars_index>(oldData));
+  auto newStars = std::get<0>(std::get<stars_index>(newData));
+  std::cout << "old stars = ";
+  printStars(oldStars);
+  std::cout << "new stars = ";
+  printStars(newStars);
+#endif
+
+  // check whether new object and old object are equal by comparing
+  // serialized outputs from the old method
+#if 0
+  // serialize original object using old method
+  size_t dimBuf = slug_buffer_size(SlugOb);
+  char *buf_slug = (char *)malloc(dimBuf);
+  slug_pack_buffer(SlugOb, buf_slug);
+
   // serialize reconstructed object using old method
   size_t dimBufNew = slug_buffer_size(new_SlugOb);
   char *buf_slugNew = (char *) malloc(dimBufNew);
   slug_pack_buffer(new_SlugOb, buf_slugNew);
 
-  // write properties to stdout
-  //SlugOb->cluster->write_prop(std::cout, ASCII, 0);
-  //new_SlugOb->cluster->write_prop(std::cout, ASCII, 0);
-
-  // check whether new object and old object are equal by comparing
-  // serialized outputs from the old method
-  ASSERT_EQ(dimBuf, dimBufNew);
+  EXPECT_EQ(dimBuf, dimBufNew);
   for(size_t i=0; i < dimBuf; ++i) {
     ASSERT_EQ(buf_slug[i], buf_slugNew[i]) << i;
   }
-  std::cout << "Passed buffer check!" << std::endl;
+#endif
 
-  // check whether old and new object are equal using operator==
-  ASSERT_EQ((*new_SlugOb->cluster), (*SlugOb->cluster));
-  std::cout << "Passed operator== check!" << std::endl;
 }
--- src/sfr.h
+++ src/sfr.h
@@ -6,6 +6,47 @@
 
 #include "slug_object.h"
 
+// template magic
+
+// Iterate over 2 tuples simultaneously, e.g.
+//  for_each2(tuple1, tuple2, [](auto&& i, auto&& s){ std::cout << i << '-' << s << '\n'; });
+//  [credit: https://www.fluentcpp.com/2019/03/08/stl-algorithms-on-tuples/]
+
+template <class Tuple1, class Tuple2, class F, std::size_t... I>
+F for_each2_impl(Tuple1 &&t1, Tuple2 &&t2, F &&f, std::index_sequence<I...>)
+{
+  return (void)std::initializer_list<int>{(std::forward<F>(f)(I, std::get<I>(std::forward<Tuple1>(t1)), std::get<I>(std::forward<Tuple2>(t2))), 0)...}, f;
+}
+template <class Tuple1, class Tuple2, class F>
+constexpr decltype(auto) for_each2(Tuple1 &&t1, Tuple2 &&t2, F &&f)
+{
+  return for_each2_impl(std::forward<Tuple1>(t1), std::forward<Tuple2>(t2), std::forward<F>(f),
+                       std::make_index_sequence<std::tuple_size<std::remove_reference_t<Tuple1>>::value>{});
+}
+
+// Get a string representation of a type at compile-time.
+//  [credit: https://stackoverflow.com/a/56766138]
+
+#include <string_view>
+template <typename T>
+constexpr auto type_name() noexcept {
+  std::string_view name, prefix, suffix;
+#ifdef __clang__
+  name = __PRETTY_FUNCTION__;
+  prefix = "auto type_name() [T = ";
+  suffix = "]";
+#elif defined(__GNUC__)
+  name = __PRETTY_FUNCTION__;
+  prefix = "constexpr auto type_name() [with T = ";
+  suffix = "]";
+#endif
+  name.remove_prefix(prefix.size());
+  name.remove_suffix(suffix.size());
+  return name;
+}
+
+// function declarations
+
 auto test_slug() -> bool;
 
 #endif // SFR_H_
